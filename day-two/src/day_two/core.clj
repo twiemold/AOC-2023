7ac7f7b2d77aba1b7d300969a99f14071f181248
Thomas Wiemold <thomas.wiemold@mascomabank.com>
2023-12-02 22:02:04 -0500

    Refactor day two

--- day-two/src/day_two/core.clj
+++ day-two/src/day_two/core.clj
@@ -3,10 +3,10 @@
 
 ;; this could be simplified ...
 (defn create-counts [data]
-  {"game-id" (reduce + (map parse-long (filter parse-long (flatten (filter some? (map #(re-find #"(\d+)\:" %) data))))))
-   "green" (reduce max (map parse-long (filter parse-long (flatten (filter some? (map #(re-find #"(\d+)\s\bgreen\b" %) data))))))
-   "blue" (reduce max (map parse-long (filter parse-long (flatten (filter some? (map #(re-find #"(\d+)\s\bblue\b" %) data))))))
-   "red" (reduce max (map parse-long (filter parse-long (flatten (filter some? (map #(re-find #"(\d+)\s\bred\b" %) data))))))})
+  {"game-id" (first (map parse-long (filter parse-long (flatten (re-seq #"(\d+)\:" data)))))
+   "green" (reduce max (map parse-long (filter parse-long (flatten (re-seq #"(\d+)\s\bgreen\b" data)))))
+   "blue" (reduce max (map parse-long (filter parse-long (flatten (re-seq #"(\d+)\s\bblue\b" data)))))
+   "red" (reduce max (map parse-long (filter parse-long (flatten (re-seq #"(\d+)\s\bred\b" data)))))})
 
 ;; part one
 (defn check-counts [color-map]
@@ -16,7 +16,8 @@
     0
     (get color-map "game-id")))
 
-(defn check-counts-v2 [color-map]
+;; part two
+(defn get-cube-product [color-map]
   (* (get color-map "red")
      (get color-map "green")
      (get color-map "blue")))
@@ -24,13 +25,11 @@
 
 (defn check-games* []
   (->> (line-seq (clojure.java.io/reader "input.txt"))
-       (map #(re-seq #"\d+\:|\d+\s\w+|\d+\s\w+" %))
        (map #(create-counts %))
-       (map #(check-counts-v2 %))
-       (reduce +)
-       (println)))
+       (map #(get-cube-product %))
+       (reduce +)))
 
 (defn -main
   "I don't do a whole lot."
   []
-  (check-games*))
+  (println (check-games*)))
