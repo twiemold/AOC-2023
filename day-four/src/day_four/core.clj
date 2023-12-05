896380ef026ef861990f9024951c38fa3c2d8309
Thomas Wiemold <thomas.wiemold@mascomabank.com>
2023-12-05 01:29:17 -0500

    Solve day four part two

--- day-four/src/day_four/core.clj
+++ day-four/src/day_four/core.clj
@@ -1,15 +1,22 @@
-(ns day-four.core
-  (:require [clojure.set :as s]))
+(ns day_four.core
+  (:require [clojure.set :as s])
+  (:gen-class))
 
 (defn exp [x n]
   (reduce * (repeat n x)))
 
-(defn parse-nums [str]
-  (re-seq #"\d+" str))
+(defn parse-nums [my-str]
+  (map parse-long (re-seq #"\d+" my-str)))
 
-(defn create-sets [[_ winning-nums-str my-nums-str]]
-  [(into #{} (parse-nums winning-nums-str)) (into #{} (parse-nums my-nums-str))])
+(defn create-sets [data]
+  (into {}
+        (map #(vector (parse-long (nth % 1))
+                      {:winning-set (into #{} (parse-nums (nth % 2)))
+                       :my-set (into #{} (parse-nums (nth % 3)))})
+             data))
+  )
 
+;; part one
 (defn count-winners [[winning-set my-set]]
   (let [matches (count (s/intersection winning-set my-set))]
     (if (> matches 0)
@@ -17,14 +24,38 @@
      0))
   )
 
+(defn count-winners-v2 [master-card-map to-be-processed counter]
+  (if (empty? to-be-processed)
+    counter
+    (let [card (first to-be-processed)
+          card-num (get card 0)
+          sets (get card 1)
+          matches (count (s/intersection (get sets :winning-set) (get sets :my-set)))]
+      (if (> matches 0)
+        (recur master-card-map
+               (concat (rest to-be-processed)
+                       (map #(vector % (get master-card-map %))
+                            (range (+ card-num 1)
+                                   (+ card-num 1 matches))))
+               (inc counter))
+        (recur master-card-map (rest to-be-processed) (inc counter)))
+      )
+    )
+  )
+
+;; recursive function with signature of master-card-vec and to be processed vec
+;; take the first off the top, count the winnings
+;; add the range of cards in the recur
+;; base case- to be processed vec is empty
+
 (defn count-winnings* []
   (->> (slurp "input.txt")
-       (re-seq #"\bCard\b\s+\d+\:\s+((?>\d+\s+)+)\|\s+((?>\d+(?>\s+|$))+)")
-       (map #(create-sets %))
-       (map #(count-winners %))
-       (reduce +)))
+       (re-seq #"\bCard\b\s+(\d+)\:\s+((?>\d+\s+)+)\|\s+((?>\d+(?>\s+|$))+)")
+       (create-sets)
+       (#(count-winners-v2 % (seq %) 0))
+       ))
 
 (defn -main
   "I don't do a whole lot."
-  []
+  [& args]
   (println (count-winnings*)))
