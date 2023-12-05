0c53aa6cf3afd63c9068e34c1e6b201c1a9496e2
Thomas Wiemold <thomas.wiemold@mascomabank.com>
2023-12-05 01:35:52 -0500

    Solve day four part two

--- day-four/src/day_four/core.clj
+++ day-four/src/day_four/core.clj
@@ -1,4 +1,4 @@
-(ns day_four.core
+(ns day-four.core
   (:require [clojure.set :as s])
   (:gen-class))
 
@@ -30,29 +30,22 @@
     (let [card (first to-be-processed)
           card-num (get card 0)
           sets (get card 1)
-          matches (count (s/intersection (get sets :winning-set) (get sets :my-set)))]
+          matches (count (s/intersection (get sets :winning-set) (get sets :my-set)))
+          new-cards (map #(vector % (get master-card-map %)) (range (+ card-num 1) (+ card-num 1 matches)))
+]
       (if (> matches 0)
-        (recur master-card-map
-               (concat (rest to-be-processed)
-                       (map #(vector % (get master-card-map %))
-                            (range (+ card-num 1)
-                                   (+ card-num 1 matches))))
-               (inc counter))
-        (recur master-card-map (rest to-be-processed) (inc counter)))
+        #(count-winners-v2 master-card-map (into (rest to-be-processed) new-cards) (inc counter))
+        #(count-winners-v2 master-card-map (rest to-be-processed) (inc counter)))
       )
     )
   )
 
-;; recursive function with signature of master-card-vec and to be processed vec
-;; take the first off the top, count the winnings
-;; add the range of cards in the recur
-;; base case- to be processed vec is empty
 
 (defn count-winnings* []
   (->> (slurp "input.txt")
        (re-seq #"\bCard\b\s+(\d+)\:\s+((?>\d+\s+)+)\|\s+((?>\d+(?>\s+|$))+)")
        (create-sets)
-       (#(count-winners-v2 % (seq %) 0))
+       (#(trampoline count-winners-v2 % (seq %) 0))
        ))
 
 (defn -main
