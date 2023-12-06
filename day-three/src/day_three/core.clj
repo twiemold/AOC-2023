c3639baa47bfd48f38bb3959191515413005fe5f
Thomas Wiemold <thomas.wiemold@mascomabank.com>
2023-12-06 18:17:45 -0500

    Solve day three part two

--- day-three/src/day_three/core.clj
+++ day-three/src/day_three/core.clj
@@ -18,39 +18,53 @@
                                j (range (- y 1) (+ y 2))]
                            [i j]))}))
 
-(defn get-numbers [input row-num]
-  (let [nums-map (re-pos #"\d+" input)]
-    (map #(create-number-object % row-num) nums-map)))
-
 (defn create-symbol-object [sym-map row-num]
-  ;; repeat symbols need to be accounted for
   (let [x (nth sym-map 0)
         y row-num]
     {:value (nth sym-map 1)
      :location [x y]}))
 
-(defn get-symbols [input row-num]
-  (let [symbols-map (re-pos #"[^\.\d]" input)]
-    (map #(create-symbol-object % row-num) symbols-map)))
+(defn parse-values [row-num input]
+  [(map #(create-symbol-object % row-num) (re-pos #"[^\.\d]" input))
+   (map #(create-number-object % row-num) (re-pos #"\d+" input))])
 
-(defn parse-values [input row-num]
-  [(get-symbols input row-num) (get-numbers input row-num)])
-
+;; part one
 (defn is-part-number [nums-symbols]
   (let [symbol-loc-set (into #{} (map #(get % :location) (filter #(not (integer? (get % :value))) nums-symbols)))
         nums (filter #(integer? (get % :value)) nums-symbols)]
     (filter #(> (count (s/intersection (get % :adjacent-vals) symbol-loc-set)) 0) nums)))
 
+;; the actual symbol-number association details are a tad
+;; hackish and also assume that a number is associated with
+;; no more than one symbol, which happens to be true
+;; for this input
+(defn map-syms-to-nums [nums-symbols]
+  (let [symbols (filter #(not (integer? (get % :value))) nums-symbols)
+        nums (filter #(integer? (get % :value)) nums-symbols)
+        symbol-by-loc (into {} (map #(vector (get % :location) %) symbols))
+        symbol-loc-set (into #{} (keys symbol-by-loc))]
+    (for [num nums
+          :let [adjacent-sym (s/intersection (get num :adjacent-vals) symbol-loc-set)
+                new-num (assoc num :adjacent-syms (first (map #(get symbol-by-loc %) (map vec adjacent-sym))))]
+          :when (> (count adjacent-sym) 0)]
+      new-num)
+    ))
+
+
 (defn count-engine-parts* []
   (->> (line-seq (clojure.java.io/reader "input.txt"))
        ;; fix this hardcoding (transducer?)
-       ;; (map #(get-num-indexes %2 %1) (range 140))
-       (map #(parse-values %2 %1) (range 140))
+       (map #(parse-values % %2) (range 140))
        (flatten)
-       (is-part-number)
+       (map-syms-to-nums)
+       (group-by #(get % :adjacent-syms))
+       (filter #(and (= (get (first %) :value) "*") (= (count (second %)) 2)))
+       (flatten)
+       (filter #(integer? (get % :value)))
        (map #(get % :value))
-       (reduce +)
-       ))
+       (partition 2)
+       (map #(apply * %))
+       (reduce +)))
 
 (defn -main
   "I don't do a whole lot."
